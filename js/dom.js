e84b548e46e1aca005ad0c98907a371a49353bc9
Jocob Hong <hungkaojay@gmail.com>
2024-05-23 23:42:17 +0800

    Update dom.js

--- js/dom.js
+++ js/dom.js
@@ -1,4 +1,5 @@
 export function addRecord(date, height, weight, bmi) {
+<<<<<<< HEAD
   const row = document.createElement("tr");
   const recordTableBody = document.querySelector("#recordTable tbody");
   row.innerHTML = `
@@ -8,3 +9,20 @@ export function addRecord(date, height, weight, bmi) {
       <td>${bmi}</td>`;
   recordTableBody.appendChild(row);
 }
+=======
+  const recordTableBody = document.querySelector("#recordTable tbody");
+  const row = document.createElement("tr");
+  row.innerHTML = `
+    <td>${date}</td>
+    <td>${height}</td>
+    <td>${weight}</td>
+    <td>${bmi}</td>`;
+  recordTableBody.appendChild(row);
+}
+
+export function saveRecord(date, height, weight, bmi) {
+  const records = JSON.parse(localStorage.getItem("bmiRecords")) || [];
+  records.push({ date, height, weight, bmi });
+  localStorage.setItem("bmiRecords", JSON.stringify(records));
+}
+>>>>>>> 6e4129148910a5f5f8428f71beb48103d08944b8
